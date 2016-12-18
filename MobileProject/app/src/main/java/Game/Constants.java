1339444cb18c0a7ccaa046becd53eeef9ead6d13
SchambourgMaxime <maxime.schambourg@gmail.com>
2016-12-18 02:25:54 -0500

    no message

---
+++ MobileProject/app/src/main/java/Game/Constants.java
@@ -0,0 +1,12 @@
+package Game;
+
+/**
+ * Created by Maxime on 2016-12-17.
+ */
+
+public interface Constants {
+
+    public static final int MESSAGE_ALERT = 0;
+
+    public static final String ALERT_TITLE = "alert_title";
+}
